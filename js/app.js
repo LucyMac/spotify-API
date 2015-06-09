044f8503df87e37df38bbd646f4f61adc1edfb18
LucyMac <alessonadaykeepstheboredomaway@gmail.com>
2015-06-10 00:20:35 +0100

    fixed artist search js

--- js/app.js
+++ js/app.js
@@ -1,24 +1,19 @@
 $(function(){     
-	var spotifyAPI = 'https://api.spotify.com/v1/artists/{id}/related-artists';
+	var getArtist = 'https://api.spotify.com/v1/search?type=artist';
 	$('#artist-search').submit(function(event){
 		event.preventDefault();
 		var searchTerm = $('#my-artist').val();
-		$.getJSON(spotifyAPI, {
-	    	part: 'snippet',
-	    	key: 'AIzaSyAqGGJTTcrXv0OppL-uc0WzN_GZOMajB9Q',
-	    	q: searchTerm
-		}, function(data){
-        	$.each(data.items, function(index, value){
-				console.log(data);
-			});
+		$.ajax(getArtist, {
+			data: {
+	    		q: searchTerm
+	    	},
+	    	success: function(data){
+	        	$.each(data.artists.items, function(index, value){
+					console.log(value);
+				});
+			}
 		});
 	});
 });
-
-
-// Related artist API format: 
-// GET https://api.spotify.com/v1/artists/{id}/related-artists
-// Example:
-// https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/related-artists
 
 
